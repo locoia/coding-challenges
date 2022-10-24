3d4916a7934a24bfd8b9ca80ac716f8406af3831
John Vasquez <jvasq2@yahoo.com>
2022-10-24 16:47:04 +0200

    updated

--- Test/test_1.py
+++ Test/test_1.py
@@ -3,8 +3,6 @@ import json
 import pytest
 
 
-
-
 @pytest.fixture
 def client(request):
     gistapi.app.config['TESTING'] = True
@@ -17,15 +15,27 @@ def test_ping(client):
     assert b'pong' in rv.data
 
 
-def test_search(client):
+def test_search0(client):
     post_data = {'username': 'justdionysus', 'pattern': 'import requests'}
-    rv = client.post('/api/v1/search', 
+    rv = client.post('/api/v1/search',
                      data=json.dumps(post_data),
-                     headers={'content-type':'application/json'})
+                     headers={'content-type': 'application/json'})
     result_dict = json.loads(rv.data.decode('utf-8'))
-    expected_dict = {'status': 'success', 
+    expected_dict = {'status': 'success',
                      'username': 'justdionysus',
                      'pattern': 'import requests',
                      'matches': [['https://gist.github.com/justdionysus/65e6162d99c2e2ea8049b0584dd00912']]}
     assert result_dict == expected_dict
 
+
+def test_search1(client):
+    post_data = {'username': 'justdionysus', 'pattern': 'TerbiumLabsChallenge_1'}
+    rv = client.post('/api/v1/search',
+                     data=json.dumps(post_data),
+                     headers={'content-type': 'application/json'})
+    result_dict = json.loads(rv.data.decode('utf-8'))
+    expected_dict = {'status': 'success',
+                     'username': 'justdionysus',
+                     'pattern': 'TerbiumLabsChallenge_1',
+                     'matches': [['https://gist.github.com/justdionysus/6b2972aa971dd605f524']]}
+    assert result_dict == expected_dict
