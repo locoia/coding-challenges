2bbcda2617664b435bd1b4703fdc16b4277e9f2f
Asim Qazi <asimijaz@live.com>
2023-11-07 13:57:43 +0100

    added core implementation

--- gistapi/gistapi.py
+++ gistapi/gistapi.py
@@ -8,8 +8,25 @@ endpoint to verify the server is up and responding and a search endpoint
 providing a search across all public Gists for a given Github account.
 """
 
-import requests
-from flask import Flask, jsonify, request
+import json
+import logging
+import os
+import re
+from asgiref.wsgi import WsgiToAsgi
+from http import HTTPStatus
+
+import aiohttp
+from flask import Flask, current_app, jsonify, request
+from pydantic import ValidationError
+
+from .models import SearchRequest, SearchResult
+from .utils import fetch_gists
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+app = Flask(__name__)
+asgi_app = WsgiToAsgi(app)
 
 
 app = Flask(__name__)
@@ -21,55 +38,69 @@ def ping():
     return "pong"
 
 
-def gists_for_user(username: str):
-    """Provides the list of gist metadata for a given user.
-
-    This abstracts the /users/:username/gist endpoint from the Github API.
-    See https://developer.github.com/v3/gists/#list-a-users-gists for
-    more information.
-
-    Args:
-        username (string): the user to query gists for
-
-    Returns:
-        The dict parsed from the json response from the Github API.  See
-        the above URL for details of the expected structure.
-    """
-    gists_url = 'https://api.github.com/users/{username}/gists'.format(username=username)
-    response = requests.get(gists_url)
-    return response.json()
-
-
 @app.route("/api/v1/search", methods=['POST'])
-def search():
-    """Provides matches for a single pattern across a single users gists.
+async def search():
+    # Extracts the base URL for GitHub's Gist API from environment variables,
+    # which allows for configuration flexibility across different deployment environments.
+    GISTS_URL = os.getenv("GISTS_URL", "https://api.github.com/users/")
 
-    Pulls down a list of all gists for a given user and then searches
-    each gist for a given regular expression.
+    try:
+        post_data = request.get_json()
+        # Validate incoming JSON data against the Pydantic model to ensure it conforms to expected structure.
+        search_request = SearchRequest(**post_data)
+        compiled_pattern = re.compile(search_request.pattern)
+    except ValidationError as e:
+        # Handles cases where the input does not match the expected schema, logging the error for traceability.
+        current_app.logger.error(f"Validation error: {e.errors()}")
+        return (
+            jsonify({"status": "error", "message": e.errors()}),
+            HTTPStatus.BAD_REQUEST,
+        )
+    except (json.JSONDecodeError, KeyError) as e:
+        # Catches issues with JSON decoding or missing fields, indicating the client sent malformed data.
+        current_app.logger.error(f"Invalid request data: {e}")
+        return (
+            jsonify({"status": "error", "message": "Invalid request data"}),
+            HTTPStatus.UNPROCESSABLE_ENTITY,
+        )
+    except re.error as e:
+        # Handles invalid regex patterns, which are user input errors.
+        current_app.logger.error(f"Regex pattern error: {e}")
+        return (
+            jsonify({"status": "error", "message": "Invalid regex pattern"}),
+            HTTPStatus.BAD_REQUEST,
+        )
 
-    Returns:
-        A Flask Response object of type application/json.  The result
-        object contains the list of matches along with a 'status' key
-        indicating any failure conditions.
-    """
-    post_data = request.get_json()
+    async with aiohttp.ClientSession() as session:
+        gists_url = f"{GISTS_URL}{search_request.username}/gists"
+        try:
+            # Asynchronously fetches gists and filters them by the regex pattern provided.
+            matches = await fetch_gists(
+                session, gists_url, compiled_pattern, logger=current_app.logger
+            )
+        except RuntimeError as e:
+            # Handles exceptions such as rate limits from the GitHub API,
+            # communicating service unavailability to the client.
+            current_app.logger.error(f"Runtime error: {e}")
+            return (
+                jsonify({"status": "error", "message": str(e)}),
+                HTTPStatus.SERVICE_UNAVAILABLE,
+            )
+        except aiohttp.ClientError as e:
+            # Captures any HTTP errors encountered during API interaction.
+            status_code = (
+                e.status if hasattr(e, "status") else HTTPStatus.INTERNAL_SERVER_ERROR
+            )
+            return jsonify({"status": "error", "message": str(e)}), status_code
 
-    username = post_data['username']
-    pattern = post_data['pattern']
-
-    result = {}
-    gists = gists_for_user(username)
-
-    for gist in gists:
-        # TODO: Fetch each gist and check for the pattern
-        pass
-
-    result['status'] = 'success'
-    result['username'] = username
-    result['pattern'] = pattern
-    result['matches'] = []
-
-    return jsonify(result)
+    # Successfully returns search results wrapped in a SearchResult model.
+    result = SearchResult(
+        status="success",
+        username=search_request.username,
+        pattern=search_request.pattern,
+        matches=matches,
+    )
+    return jsonify(result.dict())
 
 
 if __name__ == '__main__':
---
+++ gistapi/models.py
@@ -0,0 +1,45 @@
+from typing import List
+
+from pydantic import BaseModel, HttpUrl, validator
+
+
+# Model for a file within a GitHub Gist
+class GistFile(BaseModel):
+    filename: str
+    type: str
+    language: str
+    raw_url: HttpUrl
+    size: int
+
+
+# Representation of a GitHub Gist
+class Gist(BaseModel):
+    url: HttpUrl
+    files: dict[str, GistFile]
+
+
+# Search parameters for Gist querying
+class SearchRequest(BaseModel):
+    username: str
+    pattern: str
+
+    # Validation to prevent empty search parameters which can lead to unnecessary API calls
+    @validator("username")
+    def username_must_not_be_empty(cls, value):
+        if not value or value.isspace():
+            raise ValueError("Username cannot be empty or just whitespace")
+        return value
+
+    @validator("pattern")
+    def pattern_must_not_be_empty(cls, value):
+        if not value or value.isspace():
+            raise ValueError("Pattern cannot be empty or just whitespace")
+        return value
+
+
+# Search results including the occurrence of the pattern within a user's Gists
+class SearchResult(BaseModel):
+    status: str
+    username: str
+    pattern: str
+    matches: List[str]
